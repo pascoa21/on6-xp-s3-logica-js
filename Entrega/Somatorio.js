6ddb705c6b1086a22379ef4f421d80f447198059
Jessyca Maria Pascoa <50502920+pascoa21@users.noreply.github.com>
2020-08-28 18:44:04 -0300

    Ajuste no enuciado

--- Entrega/Somatorio.js
+++ Entrega/Somatorio.js
@@ -1,4 +1,4 @@
-/*4. Crie uma função que receba um número e devolva uma soma progressiva. 
+/*5. Crie uma função que receba um número e devolva uma soma progressiva. 
 Por exemplo, recebendo o número 5, a função deve retornar 15, ou seja, o resultado de 1 + 2 + 3 + 4 + 5. */
 
 const read = require('readline-sync')
